a43cbd6d858a194533fbb421bf9f9cf4d40282b6
shanesarnac <shane.sarnac@colorado.edu>
2014-09-17 12:06:03 -0600

    Added another line

--- HelloWorld/src/hello.java
+++ HelloWorld/src/hello.java
@@ -4,6 +4,7 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("I like programming in Java");
+		System.out.println("Indeed! Java is fun!");
 
 	}
 
